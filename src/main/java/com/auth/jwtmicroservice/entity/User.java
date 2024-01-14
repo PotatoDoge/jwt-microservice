b71cb3d58c9cd3ee909a896149ef8475dc4c8270
bprado <bastian.prado.v@gmail.com>
2024-01-14 12:10:38 -0800

    Add config

---
+++ src/main/java/com/auth/jwtmicroservice/entity/User.java
@@ -0,0 +1,23 @@
+package com.auth.jwtmicroservice.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class User {
+
+    @Id
+    private Long id;
+    private String username;
+    private String password;
+
+}
