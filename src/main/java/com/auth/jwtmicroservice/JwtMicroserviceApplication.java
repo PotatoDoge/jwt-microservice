218a9b6a31ca650ca581a9c0548958c3e80c6da2
bprado <bastian.prado.v@gmail.com>
2024-01-14 10:51:05 -0800

    Initial commit

---
+++ src/main/java/com/auth/jwtmicroservice/JwtMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.auth.jwtmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtMicroserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwtMicroserviceApplication.class, args);
+    }
+
+}
