09b4265dedb25d40b15c45debe9bf0d58d218f33
bprado <bastian.prado.v@gmail.com>
2024-03-21 15:19:24 -0700

    Remove unused imports

--- src/main/java/com/auth/jwtmicroservice/config/ConfigProperties/AccountConfigProperties.java
+++ src/main/java/com/auth/jwtmicroservice/config/ConfigProperties/AccountConfigProperties.java
@@ -1,8 +1,6 @@
 package com.auth.jwtmicroservice.config.ConfigProperties;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
